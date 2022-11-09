7b90edf8b1a0de4f1b308e8d52a811590ebf2137
miriam_work <miriamr@iim.csic.es>
2022-11-09 13:16:04 +0100

    Add comments and rename variables for easy reading of code

--- Main_simulate_kinetics.m
+++ Main_simulate_kinetics.m
@@ -1,53 +1,67 @@
 clear all
 close all
 
-%%% fixed parameters
-Scell   = 3.52e-8;  % cm^2
-K_H     = 0.65;     % cm-1
-kd      = 0.60;     % min^-1
-km      = 7.66;     % mug/cm^2
-xx      = 1.29;     % -
-det_lim = 1;        % when number cell<10 assume 10
+%% Parameters
+% Detection limit
+det_lim = 1;                                                            % detection limit (logN<1)
+
+% For calculation of cell surface
+r       = 0.335;                                                        % [\mu m]     Half the width in Harris 2016
+L       = 1;                                                            % [\mu m]     Length with caps (2.5 with caps in Harris 2016)
+Scellmum= 2*pi*r*(2*r + L);                                             % [\mu m^2]   Cell surface
+Scell   = Scellmum/(10000)^2;                                           % [cm^2]      Cell surface in cm
+
+% Estimated parameters
+K_H     = 0.65;                                                         % [cm-1]      Adsorption rate constant (Henry's constat)
+kd      = 0.60;                                                         % [min^-1]    Maximum inactivation rate constant
+km      = 7.66;                                                         % [mug/cm^2]  Adsorbed BAC to kill the cell
+xx      = 1.29;                                                         % -           Constant Rational mode
 
 
-%%% Load experiments
-[tt,expN,expB,NLog,infor]=get_Experimental_Data(1:5,det_lim);
-init_load=[1.43251e+08	1.89526e+08	3.56886e+07	1.47264e+08 4.50e6 1.34045e+07];
 
-%%% simulate each experiment
+%% Load experiments
+[tt,expN,expB,NLog,infor]=get_Experimental_Data(1:6,det_lim);
+
+% Initial load/inoculum values. 
+init_load=[1.43251e+08,...                                              % Estimated within the expected error of the method, that is+- 0.5 logs the measure)
+    1.89526e+08,...
+    3.56886e+07,...
+    1.47264e+08,...
+    expN{5}(1),...                                                      % not estimated
+    1.34045e+07];
+
+
+%% Simulate each experiment
 for iexp=1:6
-    if iexp<6
-        N0=init_load(iexp);
-        C0=expB{iexp}(1);
-    else
-        N0=init_load(iexp);
-        C0=expB{iexp-1}(1);
-        tt{iexp}=tt{iexp-1};
-        NLog{iexp}=NLog{iexp-1};
-        expB{iexp}=expB{iexp-1};
-    end
     
+    % Initial conditions                  	                                        
+    N0=init_load(iexp);                                                 % Initial incoculum
+    C0=expB{iexp}(1);                                                   % Initial BAC dose
+
+    % Sovel ODE system
     [t,x]=ode15s(@theMODEL,0:0.25:10,[N0,0],[],N0,C0,K_H,Scell,kd,km,xx);
 
-    %%% necessary for plotting
-    results.sim.tsim{iexp}          = t;
-    ind=find(x(:,1)<det_lim);x(ind,det_lim) = 1;
-    results.sim.obs{iexp}(:,1)      = log10(x(:,1));
-    results.sim.obs{iexp}(:,2)      = C0-x(:,2)*Scell*N0;
-    results.sim.states{iexp}(:,4)   = x(:,2);
-    inputs.exps.t_s{iexp}           = tt{iexp};
-    inputs.exps.exp_data{iexp}(:,1) = NLog{iexp};
-    inputs.exps.exp_data{iexp}(:,2) = expB{iexp};
+    % necessary for plotting
+    tsim{iexp}          = t;                                            % [min]     Simulation time 
+    ind=find(x(:,1)<det_lim);x(ind,det_lim) = 1;                        % For detection limit zone
+    obs{iexp}(:,1)      = log10(x(:,1));                                % log10cfus Plot listeria in log10(cfus)
+    obs{iexp}(:,2)      = C0-x(:,2)*Scell*N0;                           % ppm       Calculate adsorbed BAC
+    states{iexp}(:,4)   = x(:,2);                                       % ppm       Free BAC
+    t_s{iexp}           = tt{iexp};                                     % [min]     Sampling times
+    exp_data{iexp}(:,1) = NLog{iexp};                                   % experimental data listeria
+    exp_data{iexp}(:,2) = expB{iexp};                                   % esperimental data free BAC
 end
 
+%% Plot results
 get_Plot
 
 
+%% The kinetic model based on adsorption
 function dx=theMODEL(t,x,N0,C0,K_H,Scell,kd,km,xx);
-N=x(1);
-Cm=x(2);
-Cm_eq=K_H*C0/(1+K_H*Scell*N0);
-dN=-kd*N^xx*(Cm^30./(Cm^30+km^30));
-dCm=30*(Cm_eq-Cm);
+N=x(1);                                                                 % cfus Listeria
+Cm=x(2);                                                              	% ppm free BAC
+Cm_eq=K_H*C0/(1+K_H*Scell*N0);                                          % Calculate adsorbed BAC in equilibrium
+dN=-kd*N^xx*(Cm^30./(Cm^30+km^30));                                     % Calculate cfus Listeria
+dCm=30*(Cm_eq-Cm);                                                      % Calculated adsorbed BAC
 dx=[dN, dCm]';
 end
--- get_Plot.m
+++ get_Plot.m
@@ -17,28 +17,28 @@ color={'b' 'c' 'r' 'm' 'k' 'k'};
 %% Plot results
 for ii=1:6
     if ii==5
-        subplot(221),plot(results.sim.tsim{ii},results.sim.obs{ii}(:,1),[color{ii},'--']);hold on
-        subplot(223),plot(results.sim.tsim{ii},results.sim.obs{ii}(:,2),[color{ii},'--']);hold on
-        subplot(224),plot(results.sim.tsim{ii},results.sim.states{ii}(:,4),[color{ii},'--']);hold on
+        subplot(221),plot(tsim{ii},obs{ii}(:,1),[color{ii},'--']);hold on
+        subplot(223),plot(tsim{ii},obs{ii}(:,2),[color{ii},'--']);hold on
+        subplot(224),plot(tsim{ii},states{ii}(:,4),[color{ii},'--']);hold on
     elseif ii==6
-        subplot(221),plot(results.sim.tsim{ii},results.sim.obs{ii}(:,1),[color{ii},'-.']);hold on
+        subplot(221),plot(tsim{ii},obs{ii}(:,1),[color{ii},'-.']);hold on
         xlabel('Contact time [min]');ylabel({'Viable  {\it L. Monocytogenes}','[log10(CFU/mL)]'});box off
-        subplot(223),plot(results.sim.tsim{ii},results.sim.obs{ii}(:,2),[color{ii},'-.']);hold on
+        subplot(223),plot(tsim{ii},obs{ii}(:,2),[color{ii},'-.']);hold on
         xlabel('Contact time [min]');ylabel('Free BAC [ppm]');box off
-        subplot(224),plot(results.sim.tsim{ii},results.sim.states{ii}(:,4),[color{ii},'-.']);hold on
+        subplot(224),plot(tsim{ii},states{ii}(:,4),[color{ii},'-.']);hold on
         xlabel('Contact time [min]');ylabel({'BAC adsorbed in',' membrane [mg/cm^2]'});box off
     else
-        subplot(221),plot(results.sim.tsim{ii},results.sim.obs{ii}(:,1),[color{ii}]);hold on
-        subplot(223),plot(results.sim.tsim{ii},results.sim.obs{ii}(:,2),[color{ii}]);hold on
-        subplot(224),plot(results.sim.tsim{ii},results.sim.states{ii}(:,4),[color{ii}]);hold on
+        subplot(221),plot(tsim{ii},obs{ii}(:,1),[color{ii}]);hold on
+        subplot(223),plot(tsim{ii},obs{ii}(:,2),[color{ii}]);hold on
+        subplot(224),plot(tsim{ii},states{ii}(:,4),[color{ii}]);hold on
     end
 end
 
-hold on;subplot(221),area(results.sim.tsim{1},2*ones(size(results.sim.tsim{1})),'FaceColor','r','FaceAlpha',.2,'EdgeAlpha',.2)
+hold on;subplot(221),area(tsim{1},2*ones(size(tsim{1})),'FaceColor','r','FaceAlpha',.2,'EdgeAlpha',.2)
 
 for ii=1:5
-    subplot(221),plot([inputs.exps.t_s{ii}],inputs.exps.exp_data{ii}(:,1)',[color{ii},'o'],'MarkerFaceColor',color{ii});hold on
-    subplot(223),plot([inputs.exps.t_s{ii}],inputs.exps.exp_data{ii}(:,2)',[color{ii},'o'],'MarkerFaceColor',color{ii});hold on
+    subplot(221),plot([t_s{ii}],exp_data{ii}(:,1)',[color{ii},'o'],'MarkerFaceColor',color{ii});hold on
+    subplot(223),plot([t_s{ii}],exp_data{ii}(:,2)',[color{ii},'o'],'MarkerFaceColor',color{ii});hold on
 end
 
 %% Plot legends
@@ -46,7 +46,7 @@ for ii=1:4
     mylegend{ii}=['Fit ',infor{ii}];
 end
 mylegend{5}='Validation:60ppm,6log';
-mylegend{6}='Validation estimating initial load';
+mylegend{6}='Validation estimating the inoculum';
 for ii=7:10
     mylegend{ii}=['Data ',infor{ii-6}];
 end
